1de2020768f54eca7c702748bd98885faa15e5d4
Eunjin Oh <ibsuny3214@naver.com>
2024-04-07 02:14:52 +0900

    #1 사탕 게임_문제풀이 4차 실패

--- WEEK2/04.cpp
+++ WEEK2/04.cpp
@@ -9,6 +9,7 @@ int main()
 {
     int N, num = 0;
     std::vector<std::string> candy;
+    std::vector<int> MAX;
     bool TF = false;
 
     std::cin >> N;
@@ -49,26 +50,29 @@ int main()
                     TF = true;
                     break;
                 }
+                MAX.push_back(max);
             }
             if (candy[j - 1][i] != candy[j][i])
             {
                 std::vector<std::string> candy_ = candy;
 
-                char temp = candy_[i][j - 1];
-                candy_[i][j - 1] = candy_[i][j];
-                candy_[i][j] = temp;
+                char temp = candy_[j - 1][i];
+                candy_[j - 1][i] = candy_[j][i];
+                candy_[j][i] = temp;
 
                 if ((max = search_max(candy_, N)) == N)
                 {
                     TF = true;
                     break;
                 }
+                MAX.push_back(max);
             }
             if (TF)
                 break;
         }
-    }
-    std::cout << max;
+    } 
+    sort(MAX.begin(), MAX.end());
+    std::cout << MAX[MAX.size()-1];
 
     return 0;
 }
@@ -85,17 +89,16 @@ int search_max(std::vector<std::string> candy, int N)
         {
             if (candy[i][j - 1] == candy[i][j])
                 max_++;
-            if(max_==N-1)
+            if (max_ == N - 1)
                 return N;
         }
         max.push_back(max_ + 1);
         max_ = 0;
         for (int j = 1; j < N; j++)
         {
-            
             if (candy[j - 1][i] == candy[j][i])
                 max_++;
-            if(max_==N-1)
+            if (max_ == N - 1)
                 return N;
         }
         max.push_back(max_ + 1);

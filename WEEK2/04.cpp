811b05c399159087fd651d85743e36b0be69e80b
Eunjin Oh <ibsuny3214@naver.com>
2024-04-07 00:28:30 +0900

    #1 사탕 게임_문제풀이 2차 실패

--- WEEK2/04.cpp
+++ WEEK2/04.cpp
@@ -3,23 +3,21 @@
 #include <string>
 #include <algorithm>
 
-int search_max(const std::vector<std::vector<std::string>> *candy, int N);
+int search_max(const std::vector<std::string> *candy, int N);
 
 int main()
 {
     int N, num = 0;
-    std::vector<std::vector<std::string>> candy;
+    std::vector<std::string> candy;
 
     std::cin >> N;
 
     while (num < N)
     {
         std::string str;
-        std::vector<std::string> vec;
 
         std::cin >> str;
-        vec.push_back(str);
-        candy.push_back(vec);
+        candy.push_back(str);
 
         num++;
     }
@@ -33,24 +31,24 @@ int main()
             for (int j = 1; j < N; j++)
             {
                 // 행에서 swap
-                if (candy[i][0][j - 1] != candy[i][1][j])
+                if (candy[i][j - 1] != candy[i][j])
                 {
-                    std::vector<std::vector<std::string>> candy_ = candy;
+                    std::vector<std::string> candy_ = candy;
                     
-                    char temp = candy_[i][0][j - 1];
-                    candy_[i][0][j - 1] = candy_[i][0][j];
-                    candy_[i][0][j] = temp;
+                    char temp = candy_[i][j - 1];
+                    candy_[i][j - 1] = candy_[i][j];
+                    candy_[i][j] = temp;
 
                     if((max = search_max(&candy_, N))==N)
                         break;
                 }
-                if(candy[j-1][0][i] != candy[j][1][i])
+                if(candy[j-1][i] != candy[j][i])
                 {
-                    std::vector<std::vector<std::string>> candy_ = candy;
+                    std::vector<std::string> candy_ = candy;
                     
-                    char temp = candy_[i][0][j - 1];
-                    candy_[i][0][j - 1] = candy_[i][0][j];
-                    candy_[i][0][j] = temp;
+                    char temp = candy_[i][j - 1];
+                    candy_[i][j - 1] = candy_[i][j];
+                    candy_[i][j] = temp;
 
                     if((max = search_max(&candy_, N))==N)
                         break;
@@ -63,17 +61,17 @@ int main()
     
 }
 
-int search_max(const std::vector<std::vector<std::string>> *candy, int N){
+int search_max(const std::vector<std::string> *candy, int N){
     int max_;
     std::vector<int> max;
     for(int i=0;i<N;i++){
         for(int j=1;j<N;j++){
             max_=0;
-            if(candy[i][0][j-1]==candy[i][0][j])
+            if(&candy[i][j-1]==&candy[i][j])
                 max_++;
             max.push_back(max_);
             max_=0;
-            if(candy[j-1][0][i]==candy[j][0][i])
+            if(&candy[j-1][i]==&candy[j][i])
                 max_++;
             max.push_back(max_);
         }

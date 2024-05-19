1e3d0fdb602e7ddab159ea0335996d5bd90a241c
Eunjin Oh <ibsuny3214@naver.com>
2024-05-19 13:20:47 +0900

    #2 음료수 얼려 먹기_문제풀이 1차 실패

---
+++ WEEK3/01.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <string>
+
+using namespace std;
+
+vector<vector<int>> ice;
+
+bool DFS(int M, int N, vector<vector<bool>> &TF)
+{
+    if (M < 0 || M > ice.size())
+        return false;
+    vector<int> vec;
+    vec = ice[M];
+
+    if (N < 0 || N > vec.size())
+        return false;
+
+    TF[M][N] = true;
+
+    if (vec[N] == 0 && TF[M][N] == false)
+    {
+        DFS(M + 1, N, TF);
+        DFS(M - 1, N, TF);
+        DFS(M, N + 1, TF);
+        DFS(M, N - 1, TF);
+
+        return true;
+    }
+
+    return false;
+}
+
+int main()
+{
+    int N, M;
+    string num;
+    int cnt = 1;
+    int icecream = 0;
+    int rst = 0;
+
+    queue<int> que;
+
+    cin >> N >> M;
+
+    vector<vector<bool>> TF(N, vector<bool>(M, false));
+
+    for (int i = 0; i < N; i++)
+    {
+        vector<int> vec;
+
+        cin >> num;
+        for (int j = 0; j < M; j++)
+        {
+            vec.push_back(int(num[i]));
+        }
+
+        ice.push_back(vec);
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < M; j++)
+        {
+            if (DFS(N, M, TF))
+                rst += 1;
+        }
+    }
+
+    cout << rst;
+
+    return 0;
+}
